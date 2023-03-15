f05320c32136843ca1e91f354294e6c0cc597fe8
tengmu <tengmu@dian.so>
2023-03-15 17:50:06 +0800

    pinrt vga buffer

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,26 @@
 #![no_main] // 禁用所有rust层级的入口点
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle]
 pub extern "C" fn _start()-> !{
     // 因为链接器会寻找一个名为"_start"的函数，所以这个函数就是入口点
     // 默认命名为'_start'
-    loop {}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {
+
+    }
 }
 
 #[panic_handler]
